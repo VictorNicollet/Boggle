c988d6e83518f95ccf0bd8e833020b1e8d4fbda4
Victor Nicollet <victor-github@nicollet.net>
2012-05-13 12:13:34 +0200

    Adjacency list

--- boggle.ml
+++ boggle.ml
@@ -22,3 +22,24 @@ let print_board b =
    sequences. *)
 type tree = N of tree list
 
+(* Adjacency list. Instead of writing down the rules, it's easier to 
+   just write it by hand. *)
+let adjacency = 
+  [|
+    [1;5;4] ;
+    [0;2;6;5;4] ;
+    [1;3;5;6;7] ;
+    [2;6;7] ;
+    [0;1;5;9;8] ;
+    [0;1;2;4;6;8;9;10] ;
+    [1;2;3;5;7;9;10;11] ;
+    [2;3;6;10;11] ;
+    [4;5;9;12;13] ;
+    [4;5;6;8;10;12;13;14] ;
+    [5;6;7;9;11;13;14;15] ;
+    [6;7;10;14;15] ;
+    [8;9;13] ;
+    [8;9;10;12;14] ;
+    [9;10;11;13;15] ;
+    [10;11;14]
+  |]
