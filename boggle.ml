0ff82e110006e991ac6de6f9c26b624db90ff496
Victor Nicollet <victor-github@nicollet.net>
2012-05-13 12:24:02 +0200

    Add memoization to tree construction

--- boggle.ml
+++ boggle.ml
@@ -46,11 +46,22 @@ let adjacency =
 
 (* Constructing the full tree, up to the maximal depth allowed by 
    the language. This version IGNORES sequences of allowed characters.
+
+   It uses memoization so that building the tree is a linear operation
+   instead of exponential (like a graph traversal would be). 
 *)
 let make_tree b = 
   let all = [0;1;2;3;4;5;6;7;8;9;10;11;12;13;14;15] in
+  let memo = Array.init Data.longest (fun _ -> Array.make 16 None) in
   let rec aux depth node = 
     if depth = Data.longest then N (node,[]) else
-      N (node, List.map (aux (succ depth)) adjacency.(node)) 
+      match memo.(depth).(node) with 
+	| None -> 
+	  let list = List.map (aux (succ depth)) adjacency.(node) in
+	  let tree = N (node, list) in 
+	  memo.(depth).(node) <- Some tree ; tree
+	| Some tree -> tree
   in
   List.map (aux 0) all
+
+
